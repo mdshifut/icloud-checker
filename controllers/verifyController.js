8ba60b71bc29bf718bea46415cc927edb352b675
Md Shifut Hossain <mdshifut@gmail.com>
2019-11-15 11:55:33 +0600

    added apple fd client

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -17,7 +17,16 @@ exports.verifyICloudId = async (req, res) => {
         "Sec-Fetch-Mode": " cors",
         "Sec-Fetch-Site": "same-origin",
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3952.0 Safari/537.36 Edg/80.0.320.5"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3952.0 Safari/537.36 Edg/80.0.320.5",
+        "X-Apple-I-FD-Client-Info": {
+          U:
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3952.0 Safari/537.36 Edg/80.0.320.5",
+          L: "en-US",
+          Z: "GMT+06:00",
+          V: "1.1",
+          F:
+            "Nda44j1e3NlY5BSo9z4ofjb75PaK4Vpjt.gEngMQEjZr_WhXTA2s.XTVV26y8GGEDd5ihORoVyFGh8cmvSuCKzIlnY6xljQlpRDukqwoxY9hypZHgfLMC7ETIhDSpWhtx3DOfeLd7FmrpwoNN5uQ4s5uQ.gEx4xUC541jlS7spjt.gEngMQEjZr_WhXTA2s.XTVV26y8GGEDd5ihORoVyFGh8cmvSuCKzIlnY6xljQlpRDukqwoxY9hypZHgfLMC7ETIhDSpWhtx3DOfwvw0BpUMnGWqbyATblVnmccbguaDeyjaY2ftckuyPBDjaY1HGOg3ZLQ0IHd_v5AqKqvB.i.uJtHoqvynx9MsFyxYMAqJkL6f1BSp.jVH0Y.Z20Hb4BRcWqrTJfwjOyOMl7pp0iMgdVg8a53kY5BNlrJNNlY5QB4bVNjMk.39z"
+        }
       }
     }
   );
