43ca18baf14fa9c0b34f77ef9bfcd7afcfca8143
Md Shifut Hossain <mdshifut@gmail.com>
2019-11-15 10:01:07 +0600

    Add console

--- middleware/error.js
+++ middleware/error.js
@@ -2,6 +2,8 @@ const logger = require("../logger/logger");
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
+  console.log(err);
+
   logger.error(err);
   return res.status(500).json({ error: { message: "Internal server error" } });
 };
