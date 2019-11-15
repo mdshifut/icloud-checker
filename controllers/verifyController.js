6df69b01b3e8b855811da458f4780e6655bff1e0
Md Shifut Hossain <mdshifut@gmail.com>
2019-11-15 11:51:41 +0600

    add user agent

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -15,7 +15,9 @@ exports.verifyICloudId = async (req, res) => {
         Origin: "https://iforgot.apple.com",
         Referer: " https://iforgot.apple.com/password/verify/appleid",
         "Sec-Fetch-Mode": " cors",
-        "Sec-Fetch-Site": "same-origin"
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3952.0 Safari/537.36 Edg/80.0.320.5"
       }
     }
   );
